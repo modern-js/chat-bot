24cd7bb9dac66f6f1394c8422f0407c128dca554
Dimitar Kumanov <dimitar_kumanov@yahoo.com>
2017-10-20 00:39:12 +0300

    Fix dialogflow session id

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -5,22 +5,30 @@ const CAT_IMAGE_URL = 'https://upload.wikimedia.org/wikipedia/commons/d/df/Doge_
 const apiAiClient = require('apiai')(API_AI_TOKEN);
 const request = require('request');
 
-module.exports = (event) => {
-    const senderId = event.sender.id;
-    const message = event.message.text;
-
+const sendTextMessage = (senderId, text) => {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: { access_token: FACEBOOK_ACCESS_TOKEN },
         method: 'POST',
         json: {
             recipient: { id: senderId },
-            message: {
-                attachment: {
-                    type: 'image',
-                    payload: { url: CAT_IMAGE_URL}
-                }
-            }
+            message: { text },
         }
     });
+};
+
+module.exports = (event) => {
+    const senderId = event.sender.id;
+    const message = event.message.text;
+
+    const apiaiSession = apiAiClient.textRequest(message, {sessionId: 'test-bot-2f5e2'});
+
+    apiaiSession.on('response', (response) => {
+        const result = response.result.fulfillment.speech;
+
+        sendTextMessage(senderId, result);
+    });
+
+    apiaiSession.on('error', error => console.log(error));
+    apiaiSession.end();
 };
