a17f8bfbcdfdd06674a82e2d8291166e615b19ca
Dimitar Kumanov <dimitar_kumanov@yahoo.com>
2017-10-19 23:07:13 +0300

    Change default port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(5000, () => console.log('Webhook server is listening, port 5000'));
+app.listen((process.env.PORT || 5000), () => console.log('Webhook server is listening, port 5000'));
 
 app.get('/', verificationController);
 app.post('/', messageWebhookController);
