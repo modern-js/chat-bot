0dbd0c286849485194c6a570fc41f4092d0f89ca
Dimitar Kumanov <dimitar_kumanov@yahoo.com>
2017-10-20 10:28:45 +0300

    Use MS Image Search

--- controllers/imageSearch.js
+++ controllers/imageSearch.js
@@ -1,24 +1,25 @@
 const GETTY_IMAGES_API_KEY = process.env.GETTY_IMAGES_API_KEY;
+const MS_IMAGES_API_KEY = process.env.Ocp_Apim_Subscription_Key;
 
 const request = require('request');
 
 module.exports = (req, res) => {
     if (req.body.result.action === 'image') {
         const imageName = req.body.result.parameters['image_name'];
-        const apiUrl = 'https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=best&phrase=' + imageName;
+        const apiUrl = 'https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=' + imageName;
 
         request({
             uri: apiUrl,
             methos: 'GET',
-            headers: {'Api-Key': GETTY_IMAGES_API_KEY}
+            headers: {'Ocp-Apim-Subscription-Key': MS_IMAGES_API_KEY}
         }, (err, response, body) => {
             console.log(body);
-            const imageUri = JSON.parse(body).images[0].display_sizes[0].uri;
+            const imageUri = JSON.parse(body).value[0].contentUrl;
             const imageUrl = imageUri.substring(0, imageUri.indexOf('?'));
 
             return res.json({
-                speech: imageUrl,
-                displayText: imageUrl,
+                speech: imageUri,
+                displayText: imageUri,
                 source: 'image_name'
             });
         })
